774b6f6b952bdefb0a9692fd8fc773315639c2eb
Corinne Teeter <corinnet@alleninstitute.org>
2018-07-02 15:17:19 -0700

    bug fixed; electrode id conversion

--- analyses/single_psp_fitting.py
+++ analyses/single_psp_fitting.py
@@ -40,7 +40,7 @@ for connection in selected_expt_list.connection_summary():
     
         # convert cell ID to headstage ID
         pre_electrode_id = pre_cell_id - 1
-        post_electrode_id = pre_cell_id - 1
+        post_electrode_id = post_cell_id - 1
 
         # how much padding to include when extracting events
         pre_pad = 10e-3, 
@@ -69,45 +69,46 @@ for connection in selected_expt_list.connection_summary():
                 print("Skipping %s electrode ids %d, %d; no spike on first pulse" % (expt.uid, pre_electrode_id, post_electrode_id))                
                 continue
             
-#            #--for visualization to see what spike traces look like.  Can delete when done--- 
-#            else: # selects the first pulse data
-#                for pulse in spike_data:
-#                    if pulse['pulse_n'] == 1:
-#                        first_pulse_dict = pulse
-#            
-#            #plotting to see what the spikes look like; can delete when happy
-#            plt.figure()
-#            plt.subplot(3,1,1)
-#            plt.plot(first_pulse_dict['command'].data)
-#            plt.title('command')
-#            plt.subplot(3,1,2)
-#            plt.plot(first_pulse_dict['pre_rec'].data)
-#            plt.title('pre synaptic response')
-#            plt.subplot(3,1,3)
-#            plt.plot(first_pulse_dict['response'].data)
-#            plt.title('post synaptic response')
-#            plt.show()
+            #--for visualization to see what spike traces look like.  Can delete when done--- 
+            else: # selects the first pulse data
+                for pulse in spike_data:
+                    if pulse['pulse_n'] == 1:
+                        first_pulse_dict = pulse
+            
+            #plotting to see what the spikes look like; can delete when happy
+            plt.figure()
+            plt.subplot(3,1,1)
+            plt.plot(first_pulse_dict['command'].data)
+            plt.title('command')
+            plt.subplot(3,1,2)
+            plt.plot(first_pulse_dict['pre_rec'].data)
+            plt.title('pre synaptic response')
+            plt.subplot(3,1,3)
+            plt.plot(first_pulse_dict['response'].data)
+            plt.title('post synaptic response')
+            plt.annotate('uid %s, pre/post electrodes %d, %d, sweep %d' % (expt.uid, pre_electrode_id, post_electrode_id, sweep_id),  
+                         xy = (.5, .97), ha = 'center', fontsize = 16, xycoords = 'figure fraction')
+            plt.show()
             #---------------------------------------------------------------------------------
 
             # ----Note that we may want to put a induction frequency filter here---
             
-            # get the first pulses
-            #TODO: why is the following returning more than one pulse
-            post_cell_trace_view, baseline_trace_view, pre_cell_trace_view, command_trace_view = analyzer.get_train_response(pre_rec,
-                                                                                                                            post_rec, 
-                                                                                                                            start_pulse=0, 
-                                                                                                                            stop_pulse=0, 
-                                                                                                                            padding=(-10e-3, 50e-3)) #notice that instead of skipping traces without a spike at first pulse you could set which pule to use here 
-            
-            # plot what is coming out of the get train responses.
-            plt.figure()
-            plot_trace_views([4,1], post_cell_trace_view, title = 'Post synaptic response?')
-            plot_trace_views([4,4], baseline_trace_view, title = 'Baseline')
-            plot_trace_views([4,2], pre_cell_trace_view, title = 'Pre synaptic resonse')
-            plot_trace_views([4,3], command_trace_view, title = 'Current injection')
-            plt.tight_layout()
-            plt.annotate('uid %s, pre/post electrodes %d, %d, sweep %d' % (expt.uid, pre_electrode_id, post_electrode_id, sweep_id),  
-                         xy = (.5, .97), ha = 'center', fontsize = 16, xycoords = 'figure fraction')
-            plt.show()
-            print ('hi')
+#            # get the first pulses: dont need this since get_spike_responses() returns necessary responses
+#            post_cell_trace_view, baseline_trace_view, pre_cell_trace_view, command_trace_view = analyzer.get_train_response(pre_rec,
+#                                                                                                                            post_rec, 
+#                                                                                                                            start_pulse=0, 
+#                                                                                                                            stop_pulse=0, 
+#                                                                                                                            padding=(-10e-3, 50e-3)) #notice that instead of skipping traces without a spike at first pulse you could set which pule to use here 
+#            
+#            # plot what is coming out of the get train responses.
+#            plt.figure()
+#            plot_trace_views([4,1], post_cell_trace_view, title = 'Post synaptic response?')
+#            plot_trace_views([4,4], baseline_trace_view, title = 'Baseline')
+#            plot_trace_views([4,2], pre_cell_trace_view, title = 'Pre synaptic resonse')
+#            plot_trace_views([4,3], command_trace_view, title = 'Current injection')
+#            plt.tight_layout()
+#            plt.annotate('uid %s, pre/post electrodes %d, %d, sweep %d' % (expt.uid, pre_electrode_id, post_electrode_id, sweep_id),  
+#                         xy = (.5, .97), ha = 'center', fontsize = 16, xycoords = 'figure fraction')
+#            plt.show()
+#            print ('hi')
             
