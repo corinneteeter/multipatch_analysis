58e141746ca6d7c511ae17d9facb1359d18b3a15
Corinne <corinnet@alleninstitute.org>
2019-05-07 17:30:57 -0700

    small changes

--- analyses/point_and_click_nrmse_amp.py
+++ analyses/point_and_click_nrmse_amp.py
@@ -57,8 +57,9 @@ def plot_afpf_trace(scatterplot, points):
     afpf, pair = points[0].data()  #this accesses the data binded in *setData* for the selected points
 
     # create the plot window and add the waveforms to it
-    fit_plot = pg.plot()
-
+    post_plot = pg.plot()
+    pre_plot = pg.plot()
+    
     # Get the individual plots that went into this data
     s = db.Session()
     synapse_type = pair.connection_strength.synapse_type
@@ -76,24 +77,41 @@ def plot_afpf_trace(scatterplot, points):
         start_time = pulse_response.start_time  
         spike_time = pulse_response.stim_pulse.spikes[0].max_dvdt_time      
         time_before_spike = 10.e-3  
-        pr_trace = Trace(data=pulse_response.data, t0= start_time-spike_time+time_before_spike, sample_rate=db.default_sample_rate).time_slice(start=0, stop=None) #start of the data is the spike time
+        post_trace = Trace(data=pulse_response.data, t0= start_time-spike_time+time_before_spike, sample_rate=db.default_sample_rate).time_slice(start=0, stop=None) #start of the data is the spike time
+        pre_trace =Trace(data=pulse_response.stim_pulse.data, t0= start_time-spike_time+time_before_spike, sample_rate=db.default_sample_rate).time_slice(start=0, stop=None)
 
-        PCI = pg.PlotCurveItem(pr_trace.time_values*1.e3, pr_trace.data*1.e3, pen=.5)
-        fit_plot.addItem(PCI)
+        # plot post synaptic data
+        PCI = pg.PlotCurveItem(post_trace.time_values*1.e3, post_trace.data*1.e3, pen=.5)
+        post_plot.addItem(PCI)
+
+        # plot pre synaptic data                                                                                                                                                                          
+        PCI = pg.PlotCurveItem(pre_trace.time_values*1.e3, pre_trace.data*1.e3, pen=.5)
+        pre_plot.addItem(PCI)
+
+    pre_plot.setLabels(bottom='time (mS)', left='voltage (mV)',
+                        top=('Pre Synaptic Spikes: %.3f, %i to %i, n=%i, %s to %s' % (pair.experiment.acq_timestamp,
+                                                            pair.pre_cell.ext_id,
+                                                            pair.post_cell.ext_id,
+                                                            len(afpf.ic_pulse_ids),
+                                                            pair.pre_cell.cre_type,
+                                                            pair.post_cell.cre_type)))
+        
 
     # add average data and fit 
     data_trace = afpf.ic_avg_psp_data
     data_trace = Trace(data=(data_trace), sample_rate=db.default_sample_rate)
-    data_trace = pg.PlotCurveItem(data_trace.time_values*1.e3, data_trace.data*1.e3, pen='y')
-
+    data_trace = pg.PlotCurveItem(data_trace.time_values*1.e3, data_trace.data*1.e3)
+    data_trace.setPen('y', width=4)
+    
     fit_trace = afpf.ic_avg_psp_fit
     fit_trace = Trace(data=(fit_trace), sample_rate=db.default_sample_rate)
     fit_trace = pg.PlotCurveItem(fit_trace.time_values*1.e3, fit_trace.data*1.e3, pen='r')
+    fit_trace.setPen('r', width=4)
 
-    fit_plot.addItem(data_trace)
-    fit_plot.addItem(fit_trace)
-    fit_plot.setLabels(bottom='time (mS)', left='voltage (mV)', 
-                        top=('%.3f, %i to %i, nrmse=%.1f, amp=%.2f, n=%i, %s to %s' % (pair.experiment.acq_timestamp, 
+    post_plot.addItem(data_trace)
+    post_plot.addItem(fit_trace)
+    post_plot.setLabels(bottom='time (mS)', left='voltage (mV)', 
+                        top=('Post Synaptic Response: %.3f, %i to %i, nrmse=%.1f, amp=%.2f, n=%i, %s to %s' % (pair.experiment.acq_timestamp, 
                                                             pair.pre_cell.ext_id,
                                                             pair.post_cell.ext_id,
                                                             afpf.ic_nrmse, 
@@ -108,7 +126,7 @@ def plot_afpf_trace(scatterplot, points):
 # print('Clicked:' '%s' % pair)    
 
 
-#pg.plot(nrmse, amp, pen=None, symbol='o')
+# Make clickable scatter plot
 s_plot=pg.plot() #create the plot window
 s_plot.addItem(scatter) #add the scatter plot data from above
 s_plot.setLabels(left='nrmse', bottom = 'fit amplitude (mV)')
