the weighting for table 5 did not work so there is a bug here.  before after nrmse analysis added

multipatch_analysis/fit_average_first_pulse.py
@@ -293,7 +293,7 @@ def fit_average_first_pulses(pair):
 
         weight_i = np.ones(len(avg_psp_i.data)) * 10.  #set everything to ten initially
         weight_i[int((time_before_spike+.0001+xoffset) / avg_psp_i.dt):int((time_before_spike+.0001+xoffset+4e-3) / avg_psp_i.dt)] = 30.  #area around steep PSP rise 
-        weight_i[np.where(fake.time_values < min(pulse_starts))] = 20. #set baseline region to a higher weight
+        weight_i[np.where(fake.time_values < min(pulse_starts))] = 1. #set baseline region to a higher weight
         weight_i[np.isnan(fake.data)] = 0.   #area around stim artifact note that since this is spike aligned there will be some blur in where the cross talk is
 
         # fit trace