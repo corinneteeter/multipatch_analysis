3e2ae64eac5a8ab7c85043b08524e15e34bc8e50
Corinne <corinnet@alleninstitute.org>
2019-06-16 19:17:52 -0700

    spike detection in pulse duration and termination zone working. might want to consider spike initiation zone: ex in 1516233523.013, 6, 7

--- analyses/find_spikes.py
+++ analyses/find_spikes.py
@@ -19,14 +19,14 @@ from scipy.optimize import curve_fit
 
 # cells with currently poorly identified spikes 
 cell_ids = [#[1544582617.589, 1, 8, 5654136, 5654045],  #this is a good text bc two fail but the others are sort of sad looking.
-         [1544582617.589, 1, 6, 5654136, 5654045], 
-#         [1497417667.378, 5, 2, 7483977, 7483912],
-#         [1491942526.646, 8, 1, 6693052, 6693000],
+         #[1544582617.589, 1, 6, 5654136, 5654045], 
+         #[1497417667.378, 5, 2, 7483977, 7483912],
+         #[#1491942526.646, 8, 1, 6693052, 6693000],  #this presynaptic cell is sick.  Spiking is ambiguious, very interesting examples
 #         [1521004040.059, 5, 6],
-#         [1534293227.896, 7, 8], #this one stuff spikes at end
+         #[31534293227.896, 7, 8, 7271530], #this one stuff spikes at end
 #         [1540356446.981, 8, 6],
 #         [1550101654.271, 1, 6], # these spike toward the end and are found correctly
-         [1516233523.013, 6, 7],  #spikes happen extreemly early this might require saying that there is a spike it it is driven above a certain amount.
+         [1516233523.013, 6, 7],  #very interesting example: a voltage deflection happens very early but cant be seen in dvvdt due to to onset being to early.  Think about if there is a way to fix this.  Maybe and initial pulse window.  
          [1534297702.068, 7, 2]
             ]
 
@@ -41,7 +41,7 @@ for cell_id in cell_ids:
     pre_syn_times = []
     pre_syn_voltages = []
     for pr in pulse_responses:
-        if pr.stim_pulse_id in cell_id[3:]: #ic_pulse_ids:  
+        if pr.stim_pulse_id in ic_pulse_ids: # cell_id[3:]: # 
             
             print(synapse, synapse_type, ', pass ex qc', pr.ex_qc_pass)
             print(synapse, synapse_type, ', pass in qc', pr.in_qc_pass)
@@ -59,12 +59,17 @@ for cell_id in cell_ids:
 #            pre_inj_current = Trace(data=pr.stim_pulse.data, t0= start_time-spike_time+time_before_spike, sample_rate=db.default_sample_rate).time_slice(start=0, stop=None)
             pre_syn_times.append(pre_voltage.time_values)
             pre_syn_voltages.append(pre_voltage.data)
+            time = pre_syn_times[-1]
             dvdt = np.diff(pre_syn_voltages[-1])
+            dvdt_time = time[1:]
             dvvdt = np.diff(dvdt)
+            dvvdt_time = time[2:]
 
+            #-----------------------------------------------------
             #----------spike detection----------------------------
+            #-----------------------------------------------------
             # look for max of dvvdt in region during pulse and eliminating region where current injection artifact 
-            pulse_window =np.where((pre_syn_times[-1][2:] > (pulse_start_time + .0003)) & (pre_syn_times[-1][2:] < (pulse_end_time - .0002)))[0]
+            pulse_window =np.where((dvvdt_time > (pulse_start_time + .0003)) & (dvvdt_time < (pulse_end_time - .0002)))[0]
             max_dvvdt = np.max(dvvdt[pulse_window]) #this 
                 
             max_index = np.where(dvvdt==max_dvvdt)
@@ -74,15 +79,19 @@ for cell_id in cell_ids:
                 max_index=max_index[0][0]
 
             if max_dvvdt > .5e-3:
-               spike_found = True
-            else:
-                spike_found = False
+                spike_index = max_index + 2 #converting to voltage space  
 
-            # now look in window where pulse is terminated to see if it spikes (hard to differentiate)
-            pulse_end_window = np.where((pre_syn_times[-1][2:] > (pulse_end_time - .0002)) & (pre_syn_times[-1][2:] < (pulse_end_time + .0005)))[0]
+            else:
+                spike_index = None
+            # Note that there is slop in the time indexing due to dvdt and dvvdt but it would be surprising if it ever mattered
+            # --now look in window where pulse is terminated to see if it spikes (hard to differentiate)----
+            # note that if it does not spike the trace will be similar to an exponential starting at the 
+            # min of dv/dt in the pulse termination window.---
+            pulse_end_window = np.where((dvvdt_time > (pulse_end_time - .0002)) & (dvvdt_time < (pulse_end_time + .0005)))[0] 
             #find location of minimum dv/dt
-            min_dvdt = np.min(dvdt[pulse_end_window]) #this 
-            min_index = np.where(dvdt==min_dvdt)
+            min_dvdt = np.min(dvdt[pulse_end_window-1]) #-1 because now using dvdt
+            min_index = np.where(dvdt==min_dvdt) #note that this index is relative to whole trace not just pulse termination window      
+            
             if len(min_index) > 1:
                 raise Exception('should only be one min')
             else: 
@@ -93,44 +102,62 @@ for cell_id in cell_ids:
                 is no spike one would expect this to fall off as the RC of the cell. """
                 return -(Vo/tau)*np.exp(-t/tau)
 
-            ttofit=pre_syn_times[-1][(min_index+1):] #not the plus one because time trace of derivative needs to be one shorter
-            popt, pcov = curve_fit(derivative, ttofit, dvdt[min_index:])
+            ttofit=time[(min_index+1):] #note the plus one because time trace of derivative needs to be one shorter
+            dvtofit=dvdt[min_index:]
+            popt, pcov = curve_fit(derivative, ttofit, dvtofit)
+            fit = derivative(ttofit, *popt)
+            #TODO need to catch a runtime error here "RuntimeError: Optimal parameters not found: Number of calls to function has reached maxfev = 600.""
 
-            #TODO: add test to see if the fit adequately defines the function.  I should probably just define and r-squared
+            mse = (np.sum((dvtofit-fit)**2))/len(fit)*1e10 #mean squared error
+            if (mse > 500.) &  (spike_index == None):
+                print("Unidentified spike found in pulse termination window") 
+                # find min dvvdt in pulse termination window.
+                min_dvvdt = np.min(dvvdt[pulse_end_window]) #-1 because now using dvdt
+                min_index = np.where(dvvdt==min_dvvdt) #note that this index is relative to whole trace not just pulse termination window      
+                if len(min_index) > 1:
+                    raise Exception('should only be one min')
+                else: 
+                    min_index=min_index[0][0]
+                
+                spike_index = min_index + 2 #converting to voltage space
 
-            plt.figure()
-            plt.plot(ttofit, dvdt[min_index:], 'r')
-            plt.plot(ttofit, derivative(ttofit, *popt), 'k--')
-            plt.show()
+                plt.figure()
+                plt.plot(ttofit, dvtofit, 'r')
+                plt.plot(ttofit, fit, 'k--', label=('mse: %f' % (mse)))
+                plt.legend()
+            
             #------------------------------------------------------
 
             # plotting
-            plt.figure()
+            plt.figure(figsize =  (20, 10))
             
             ax1 = plt.subplot(1,1, 1)
-            ln1=ax1.plot(pre_syn_times[-1][2:]*1.e3, pre_syn_voltages[-1][2:]*1.e3, 'b', lw=2, label="data")
+            ln1=ax1.plot(time*1.e3, pre_syn_voltages[-1]*1.e3, 'b', lw=2, label="data")
             ax1.tick_params(axis='y', colors='b')
             ax1.set_ylabel('voltage (mV)', color='b')
+            ax1.axvspan(dvvdt_time[pulse_end_window[0]]*1.e3, dvvdt_time[pulse_end_window[-1]]*1.e3, facecolor = 'k', alpha=.2)
+            ax1.axvspan(dvvdt_time[pulse_window[0]]*1.e3, dvvdt_time[pulse_window[-1]]*1.e3, facecolor = 'm', alpha=.1)
+
             ax1.set_xlabel('time (ms)')
 
             ax2=ax1.twinx()
-            ln2=ax2.plot(pre_syn_times[-1][2:]*1.e3, dvdt[1:], 'r', label="dv/dt")
+            ln2=ax2.plot(dvdt_time*1.e3, dvdt, 'r', label="dv/dt")
             ax2.set_yticks([])
             
             ax3=ax1.twinx()
-            ln3=ax3.plot(pre_syn_times[-1][2:]*1.e3, dvvdt, 'g', label="dvv/dt")
+            ln3=ax3.plot(dvvdt_time*1.e3, dvvdt, 'g', label="dvv/dt")
 
             # add legend
             lns = ln1+ln2+ln3
             labs = [l.get_label() for l in lns]
             ax1.legend(lns, labs)
 
-            if spike_found:
-                ax1.axvline(pre_syn_times[-1][2:][max_index]*1.e3, linestyle= '--', color = 'k')
+            if spike_index:
+                ax1.axvline(time[spike_index]*1.e3, linestyle= '--', color = 'k')
             plt.title("%.3f, %i, %i, %i" % (cell_id[0], cell_id[1], cell_id[2], pr.stim_pulse_id))
             plt.show()
             
-        #spikes = sd.detect_putative_spikes(pre_syn_voltages[-1], pre_syn_times[-1], start=None, end=None, filter=None, dv_cutoff=20.)           
+        #spikes = sd.detect_putative_spikes(pre_syn_voltages[-1], time, start=None, end=None, filter=None, dv_cutoff=20.)           
 
 
 
